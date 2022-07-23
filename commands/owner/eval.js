1d50111d54ae7760e702935bc4802688e649477e
Rem <103377462+wtf-rem@users.noreply.github.com>
2022-07-23 07:23:33 -0500

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -12,82 +12,15 @@ module.exports = {
     /**
      *
      * @param {Client} client
-     * @param {Message} message
+     * @param {Channel} message.channel
      * @param {String[]} args
      */
      execute: async (client, message, args) => {
-         
-        //const evalmods = ["884070608181297172", "840213882147831879", "798675881819111424",
- //"963447375177875456"]                       
-        if(!client.config.devs.includes(message.author.id)) return message.delete()
-       
-         
-        const embed = new MessageEmbed()
-        embed.setAuthor(
-          "Evaluation",
-          
-        )
-            .setColor(client.embedColor)
-       embed.setDescription(`Evaluated in: *${client.ws.ping}ms*`)
-        
-        .addField("📥 Input", "```js\n" + args.join(" ") + "```");
-
-        try {
-            const code = args.join(" ");
-            if (!code) return message.channel.send("What even are you evaluating?");
-            let evaled;
-
-            if (code.toLowerCase().includes(`secret`) || code.toLowerCase().includes(`token`) ||
-          
-           code.toLowerCase().includes(`config`) ||    code.toLowerCase().includes("client.ban") ||   code.toLowerCase().includes("process.env")) {
-                evaled = "No.";
-                
-            } else {
-                evaled = await eval(code);
-            }
-
-            if (typeof evaled !== "string") evaled = await require("util").inspect(evaled, { depth: 0 });
-            
-            let output = clean(evaled);
-            if (output.length > 1024) {
-                embed.setColor(client.embedColor)
-              
-                const { body } = await post("https://hastebin.com/documents").send(output);
-                embed.addField("📤 Output", `\`\`\`Output is too long for Discord click\`\`\` **[here](https://hastebin.com/${body.key}.js)**.`)
-              embed.addField(`Type`, "```js\n" + typeof evaled + "```")
-                
-            } else {
-                embed    .setColor(client.embedColor)
-                embed.addField("📤 Output", "```js\n" + output + "```")
-                embed.addField(`Type`, "```js\n" + typeof evaled + "```")
-            }
-
-            message.channel.send({embeds: [embed]});
-
-        } catch (error) {
-            let err = clean(error);
-            if (err.length > 1024) {
-               
-                const { body } = await post("https://hastebin.com/documents").send(err);
-                embed    .setColor(client.embedColor)
-                embed.addField("📤 Output", `\`\`\`Output is too long for Discord! Link: https://hastebin.com/${body.key}.js\`\`\``)
-                embed.addField(`Type`, "```js\n" + typeof evaled + "```")
-            } else {
-                embed.addField("📤 Output", "```js\n" + err + "```")
-                embed.addField(`Type`, "```js\n" + typeof evaled + "```")
-                embed    .setColor(client.embedColor)
-            }    
-
-            message.channel.send({embeds: [embed]});
-        }
-    }
-}
-
-function clean(string) {
-    if (typeof text === "string") {
-        return string.replace(/`/g, "`" + String.fromCharCode(8203))
-            .replace(/@/g, "@" + String.fromCharCode(8203))
-    } else {
-        return string;
-    }
+     const channel = message.channel     
+         if(!client.config.devs.includes(message.author.id)) return message.delete()
+  const output = await eval(args[0]);
+   const msg = await channel.send({ content: "Evaluating code... This may take a while..." });
+   msg.edit({ content: `Here's your evaluation.\n\`\`\`js\n${output}\`\`\`` });
+   console.log(`${message.author.tag} evaluated ${args[0]}!`);
+   }
 }
