e1d88de0b6bf49838d62b4182a95cef28c9dc98d
Emke <103377462+emkie8241@users.noreply.github.com>
2022-09-10 00:47:01 -0500

    Update CustomError.ts

--- typescript/errors/CustomError.ts
+++ typescript/errors/CustomError.ts
@@ -1,6 +1,7 @@
 export const CustomError = class extends Error {
   name: string;
   message: string;
+
   constructor(name: string, message: string) {
     super(name, message);
     this.name = name; //You can also set the name as anything if you remove name: strong and make this.name = "ErrorName".
