582cb11bf8ebf58e285f442e818589693d5b4716
Emke <103377462+emkie8241@users.noreply.github.com>
2022-09-10 00:09:04 -0500

    Delete listeners directory

--- listeners/errorPrevent.js
+++
@@ -1,6 +0,0 @@
-//DONT TOUCH THIS.
-//=====================================
-const client = require('/home/container/index.js');
-client.on('error', m => console.log(m));
-process.on('uncaughtException', error => console.log(error));
-//======================================
