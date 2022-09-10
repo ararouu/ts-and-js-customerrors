28bd5bef6ae8fc52ad154714c940c24f9c407551
Emke <103377462+emkie8241@users.noreply.github.com>
2022-09-10 00:20:16 -0500

    Update index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,5 +1,7 @@
-//Importing it:
+//Importing the errors:
 import { CustomError } from "./errors/CustomError";
+import { ExampleError } from "./errors/ExampleError";
 
 //Throwing the error:
 throw new CustomError("ExampleError", "Hiya! This is a demonstration of the custom error class.");
+throw new ExampleError("Hiya! This is an example of the ExampleError. As you can see, we don't need to add a name to it!");
