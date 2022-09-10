9a63eef060a2ee4f7059284ba8631da37a15618f
Emke <103377462+emkie8241@users.noreply.github.com>
2022-09-10 00:25:52 -0500

    Create index.js

---
+++ javascript/src/index.js
@@ -0,0 +1,7 @@
+//Importing the errors:
+import { CustomError } from "./errors/CustomError.js";
+import { ExampleError } from "./errors/ExampleError.js";
+
+//Throwing the error:
+throw new CustomError("ExampleError", "Hiya! This is a demonstration of the custom error class.");
+throw new ExampleError("Hiya! This is an example of the ExampleError. As you can see, we don't need to add a name to it!");
