53b855384d15a46088d0bc9e70f7bc81078861ac
Emke <103377462+emkie8241@users.noreply.github.com>
2022-09-10 00:13:32 -0500

    Create index.ts

---
+++ typescript/src/index.ts
@@ -0,0 +1,5 @@
+//Importing it:
+import { CustomError } from "./errors/CustomError";
+
+//Throwing the error:
+throw new CustomError("ExampleError", "Hiya! This is a demonstration of the custom error class.");
