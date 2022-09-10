0baaad6f54d4eca493328bb3123c123c6e61930a
Emke <103377462+emkie8241@users.noreply.github.com>
2022-09-10 00:17:26 -0500

    Create ExampleError.js

---
+++ typescript/errors/ExampleError.js
@@ -0,0 +1,10 @@
+export const CustomError = class extends Error {
+  message: string;
+
+  constructor(message: string) {
+    super(message);
+
+    this.name = "ExampleError";
+    this.message = message; //Displays the message to when an error is thrown.
+    }
+  }
