add2bd2f203b03a55baa1e1fc81114e21b51c0bc
Emke <103377462+emkie8241@users.noreply.github.com>
2022-09-10 00:47:12 -0500

    Update ExampleError.ts

--- typescript/errors/ExampleError.ts
+++ typescript/errors/ExampleError.ts
@@ -3,7 +3,6 @@ export const ExampleError = class extends Error {
 
   constructor(message: string) {
     super(message);
-
     this.name = "ExampleError";
     this.message = message; //Displays the message to when an error is thrown.
     }
