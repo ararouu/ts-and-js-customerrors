dd859d43e82bba69b30713af5ffb6cd71e0f25ae
Karma <103377462+worst-karma@users.noreply.github.com>
2022-09-22 14:26:21 -0500

    test

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -5,3 +5,4 @@ import { ExampleError } from "./errors/ExampleError.js";
 //Throwing the error:
 throw new CustomError("ExampleError", "Hiya! This is a demonstration of the custom error class.");
 throw new ExampleError("Hiya! This is an example of the ExampleError. As you can see, we don't need to add a name to it!");
+//trol
